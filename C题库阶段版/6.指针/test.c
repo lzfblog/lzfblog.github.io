044e81a2fd755cb6cf429093dd0e6cea12439406
lzfblog <3307136503@qq.com>
2022-04-17 11:23:13 +0000

    0

--- C题库阶段版/6.指针/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void){
-    char *str=(char *)malloc(20*sizeof(char));
-    gets(str);
-    puts(str);
-    
-    return 0;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# 02_C_QuestionBankofCyberSecurity
-C语言题库(网安院)
